ed074e87efe6a1309b2f1d7f6ba4f7cb6b929df6
apoorvagni@kubuntu <apoorvagni@hotmail.com>
2019-07-19 15:25:10 +0530

    added basic gp interpolator

---
+++ polire/gp/gp.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, Matern
+
+from ..base import Base
+
+
+class GaussianProcess(Base):
+    """
+    Class to interpolate by fitting a gaussian process to given
+    data.
+    """
+
+    def __init__(self, resolution="standard", coordinate_type="Euclidean", **kwargs):
+        super().__init__(resolution, coordinate_type)
+        self.gp = GaussianProcessRegressor(normalize_y=True, kernel=Matern())
+
+    def _fit(self, X, y):
+        """Function for fitting.
+        This function is not supposed to be called directly.
+        """
+        self.gp.fit(X, y)
+        return self
+
+    def _predict_grid(self, lims):
+        """Function for grid interpolation.
+        This function is not supposed to be called directly.
+        """
+        x1min, x1max, x2min, x2max = lims
+        x1 = np.linspace(x1min, x1max, self.resolution)
+        x2 = np.linspace(x2min, x2max, self.resolution)
+        X1, X2 = np.meshgrid(x1, x2)
+        return self.gp.predict(np.array([X1.ravel(), X2.ravel()]).T)
+
+    def _predict(self, X):
+        """Function for interpolation on specific points.
+        This function is not supposed to be called directly.
+        """
+        return self.gp.predict(X)
