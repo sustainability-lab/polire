edc3f30f7a00ebcf4df0056152298dc5cb098312
deeps-nars <deepak.narayanan@iitgn.ac.in>
2019-07-29 01:42:51 +0530

    Natural Neighbors, no gridded interpolation

--- polire/natural_neighbors/natural_neighbors.py
+++ polire/natural_neighbors/natural_neighbors.py
@@ -9,10 +9,13 @@ from ..base import Base
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 from math import atan2
+from copy import deepcopy
 
 def order_poly(vertices):
     """This function essentially is used to order the vertices
-    of the Voronoi polygon in a clockwise manner. 
+    of the Voronoi polygon in a clockwise manner. This ensures
+    that Shapely doesn't produce Polygon objects that are potentially
+    non-convex and non-zero area.
     
     Arguments
     ---------
@@ -50,10 +53,11 @@ class Natural_neighbor(Base):
 		True value displays the voronoi tesselation to the user after fitting the model.
 		Default value is False.
 
-	Notes
+	Notes 
 	-----
+	This is for contributors:
 		The way in which part of the code is used is in the assumption that
-		we can 
+		we use the data's ordering to find its voronoi partitions. 
 	References
 	----------
 	[1]  Sibson, R. (1981). "A brief description of natural neighbor interpolation (Chapter 2)". In V. Barnett (ed.). Interpolating Multivariate Data. Chichester: John Wiley. pp. 21–36.
@@ -74,6 +78,7 @@ class Natural_neighbor(Base):
 		self.y = None
 		self.result = None
 		self.voronoi = None
+		self.vertices = None #This variable stored the voronoi partition's vertices
 		self.regions = [] # This variable stores the original Voronoi regions
 		self.vertex_poly_map = dict() # This variable stores the polygon to data point map
 
@@ -84,6 +89,7 @@ class Natural_neighbor(Base):
 		self.X = X
 		self.y = y
 		self.voronoi = Voronoi(X, incremental = True)
+		self.vertices = self.voronoi.vertices
 		regions = self.voronoi.regions
 
 		for i in regions:
@@ -94,16 +100,66 @@ class Natural_neighbor(Base):
 
 		self.vertex_poly_map = {i:0 for i in range(len(X))}
 		
+		# Assigning each data point to its corresponding polygon
 		for i in range(len(X)):
 			point = Point(X[i,0], X[i,1])
 			for j in range(len(self.regions)):
 				p = Polygon(order_poly(self.voronoi.vertices[self.regions[j]]))
 				if p.contains(point):
 					self.vertex_poly_map[i] = p
+		# Remove all the data points that do not contribute to Nearest Neighhbor interpolation
+		for i in range(len(vertex_poly_map)):
+			if self.vertex_poly_map[i]==0:
+				self.vertex_poly_map.pop(i,None)
 
-		# if self.display:
-		# 	voronoi_plot_2d(vor)
-		# 	plt.show()
+
+		if self.display:
+			voronoi_plot_2d(vor)
+			plt.show()
+
+	def _predict_grid(self, x1lim, x2lim):
+		raise NotImplementedError
+
+	def _predict(self, X):
+		"""The function taht is called to predict the interpolated data in Natural Neighbors
+		interpolation. This should not be called directly.
+		"""
+		result = np.zeros(len(X))
+
+		for index in range(len(X)):
+			vor = deepcopy(self.voronoi)
+			vor.add_points(np.array([X[index]]))
+			# We exploit the incremental processing of Scipy's Voronoi.
+			# We create a copy to ensure that the original copy is preserved.
+			new_regions = vor.regions
+			new_vertices = vor.vertices
+			final_regions = []
+
+			for i in new_regions:
+				if i!=[] and -1 not in i:
+					final_regions.append(i)
+
+			new = [] # this stores the newly created voronoi partitions
+			for i in range(len(new_vertices)):
+				if new_vertices[i] not in self.vertices:
+					new.append(new_vertices[i])
+			new = np.array(new)
+
+			weights = {}	#Weights that we use for interpolation
+
+			new_polygon = Polygon(order_poly(new))
+			new_polygon_area = new_polygon.area
+
+			for i in self.vertex_poly_map:
+				if new_polygon.intersects(vertex_poly_map[i]):
+					weights[i] = (new_polygon.intersection(self.vertex_poly_map[i])).area/new_polygon_area
+
+			prediction = np.array([self.y[i]*weights[i] for i in weights]).sum()
+			result[index] = prediction
+			del vor, weights, new_polygon, new_polygon_area
+
+		return result
+
 
 
 
