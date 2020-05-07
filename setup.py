90f598bf9d3f8c519abcccb735479ea170e90d6b
deeps-nars <deepak.narayanan@iitgn.ac.in>
2020-05-07 18:39:27 +0530

    Changes

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ ver_file = os.path.join('polire', '_version.py')
 with open(ver_file) as f:
     exec(f.read())
 
+HERE = os.path.abspath(os.path.dirname(__file__))
+
 with open(os.path.join(HERE, "requirements.txt"), encoding="utf-8") as f:
     REQ = f.read().splitlines()
 
