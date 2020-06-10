28e421b874abbd98f8dd017342aee4b2163edab8
deeps-nars <deepak.narayanan@iitgn.ac.in>
2020-06-10 12:39:13 +0530

    Changes to usage and add sensor placement subpackage

--- usage.py
+++ usage.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from polire.interpolate import Random, Trend, BSpline, Idw, Natural_neighbor, Kriging
+from polire.interpolate import Random, Trend, BSpline, Idw, Natural_neighbor, Kriging, SpatialAverage
 
 # sample data
 X = [[0, 0], [0, 3], [3, 0], [3, 3]]
