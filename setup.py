d88e733b6c55ca8e2dfac73d642cae1a3e096ece
patel-zeel <patel_zeel@iitgn.ac.in>
2021-07-04 15:47:52 +0530

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ import os
 
 from setuptools import find_packages, setup
 
-# get __version__ from _version.py
-ver_file = os.path.join('polire', '_version.py')
-with open(ver_file) as f:
-    exec(f.read())
-
-
 with open("requirements.txt") as f:
     REQ = f.read().splitlines()
 
